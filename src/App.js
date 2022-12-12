3285d12109365048da6470e39ed7291a01fb25db
Kazi Reazul Islam <kazireazulislam1999@gmail.com>
2022-12-12 12:58:15 +0600

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container my-5 w-50">
+      <div className="card text-center ">
+        <div className="card-body">
+          <div className="card-title my-5">
+            <h1>Counter App</h1>
+            <div className="card-count my-5">
+              <h2>{count}</h2>
+              <div className="card-button my-5">
+                <button
+                  className="btn btn-success mx-3"
+                  onClick={() => setCount(count + 1)}
+                >
+                  Increment
+                </button>
+                <button
+                  className="btn btn-warning mx-3"
+                  onClick={() => setCount(count - 1)}
+                  disabled={count === 0}
+                >
+                  Decrement
+                </button>
+                <button
+                  className="btn btn-danger mx-3"
+                  onClick={() => setCount(0)}
+                >
+                  Reset
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
